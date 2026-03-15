12c6f2bc5614678801fa57a2d782199fe155d563
Vitor Pavan <vipavani@hotmail.com>
2026-03-15 10:26:55 +0100

    feat: add multiply function with unit test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,10 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_multiply() {
+        assert_eq!(multiply(3, 4), 12);
     }
 }
