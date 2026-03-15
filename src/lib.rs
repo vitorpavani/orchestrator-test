278660ff2d9c7172a9b23c3de2331810453651b4
Vitor Pavan <vipavani@hotmail.com>
2026-03-15 10:50:49 +0100

    feat: add subtract function with unit test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,10 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_subtract() {
+        assert_eq!(subtract(5, 3), 2);
     }
 }
